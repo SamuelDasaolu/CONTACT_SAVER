db9e03520af69c7ab995664094eb9203e9a70adf
Zedprince <zacheaustaiwo2020@gmail.com>
2025-08-29 14:50:35 +0100

    commit mesg

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ from participant_pkg.file_ops import load_participants, save_participant
 workspace_path = Path("WORKSPACE_FILES")
 workspace_path.mkdir(exist_ok=True)
 file_path = workspace_path / 'contacts.csv'
+from participant_pkg.helper import get_age, get_phone,get_track,get_name
+
+while True:
+    name = get_name
+    age = get_age
+    phone_number= get_phone
+    track=get_track
+    # again = input("Add another participant? (y/n): ").lower()
+    # if again != "y":
+    #     break
+
+# After loop → show all participants
+participants = load_participants(csv_path)
+print(f"\nTotal participants saved: {len(participants)}")
+for p in participants:
+    print(p)
+
+
+
+
+
