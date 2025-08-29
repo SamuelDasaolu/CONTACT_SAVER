8b7e9c8552c0b5cc2cf638ca4d04c1c585c9de95
Simeon Akinrinola <simeonakinrinola7@gmail.com>
2025-08-29 13:43:50 +0100

    "finished beautify"

--- beautify.py
+++ beautify.py
@@ -1,7 +1,33 @@
-def beautify(string) -> str:
-    lines = string.splitlines()
-    return "\n".join(line + "\n" for line in lines)
+import json
+import csv
+import io
+
+
+test_data_type  = {'string'}
+def beautify_json_string(data, data_type ="string"):
+    #JSON
+    if data_type == "json":
+        # if data is string
+        if data.strip().startswith("{") or data.strip().startswith("["):
+            obj = json.loads(data)
+        # if data is file
+        else:
+            with open(data, "r") as f:
+                obj = json.load(f)
+        return json.dumps(obj, indent=4)
+    
+    # RAW STRING
+    else:
+        return "\n" + data.strip() + "\n"
     
 
-print(beautify(string="My name is Simeon."))
-
+def beautify_csv(data):
+    try:
+        with open(data, "r", encoding="utf-8") as f:
+            csv_f = csv.reader(f)
+        for row in csv_f:
+            print('{:<15}  {:<15}  {:<20} {:<25}'.format(*row))
+    except FileNotFoundError:
+        print(f"Error: The file '{data}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
