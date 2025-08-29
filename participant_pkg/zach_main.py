d5e8ae73495d80322d3ab6c36e7a805bf16eb11c
Zedprince <zacheaustaiwo2020@gmail.com>
2025-08-29 12:05:12 +0100

    zach_work main

--- participant_pkg/zach_main.py
+++ participant_pkg/zach_main.py
@@ -1,22 +1,3 @@
-# """
-# file_path = workspace/ contacts.csv
-# Variables to Collect: name, age(strictly integer), phone number and track
-# Validate Inputs (Phone by length, non-empty etc)
-# handling Errors (reprompt if error)
-
-# details = {name, age, phone, track}
-# allow for multiple participants etc
-
-# """
-
-# from pathlib import Path
-# from participant_pkg.file_ops import load_participants, save_participant
-
-# workspace_path = Path("WORKSPACE_FILES")
-# workspace_path.mkdir(exist_ok=True)
-# file_path = workspace_path / 'contacts.csv'
-
-
 
 # # MAIN
 # from pathlib import Path
