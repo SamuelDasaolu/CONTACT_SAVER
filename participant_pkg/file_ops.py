1daa43af7ee5818c42895a99e8aad5dc5742ac0b
SDasaolu <samueldasaolu40@gmail.com>
2025-08-29 10:17:27 +0100

    Initial Commit after project setup

---
+++ participant_pkg/file_ops.py
@@ -0,0 +1,18 @@
+import csv
+from pathlib import Path
+
+
+def save_participant(file_path: Path, participant_dictionary: dict):
+    """Update A CSV file with a Participants Details 
+    Creates A New File if it doesn't exist
+    Creates Header if It Doesn't Exist"""
+    
+
+
+def load_participants(file_path: Path) -> list:
+    """This reads all participants from the CSV and 
+    returns them as a list of dictionaries."""
+    participants = []
+    
+
+    return participants
